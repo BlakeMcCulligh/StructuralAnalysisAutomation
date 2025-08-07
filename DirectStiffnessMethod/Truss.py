73e67b00ab1b09f5958056b523f3e9804a14b88d
Blake McCulligh <blake@mcculligh.com>
2025-08-07 15:54:33 -0400

    Direct Stiffness Method for Trusses Improvments

--- DirectStiffnessMethod/Truss.py
+++ DirectStiffnessMethod/Truss.py
@@ -1,113 +1,147 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# %% Input truss structure data
-E = 1e4
-A = 0.111  # all parts have same cross section area. Try to cahnge it so a list can be inputed when the bars are getting made. same with E?. Maybe make it all a class. with methods to add nodes and bars. so they can still be stored separately
+class Truss:
+    def __init__(self):
+        self.E = 1e4
+        self.Nodes = []
+        self.Bars = []
+        self.Area = []
+        self.appliedForces = []
+        self.supports = []
+        self.Forces = []
+        self.Reactions = []
+        self.Displacements = []
 
-nodes = []
-bars = []
+    def addNode(self,x,y):
+        self.Nodes.append([x,y])
 
-nodes.append([0, 120])
-nodes.append([120, 120])
-nodes.append([240, 120])
-nodes.append([360, 120])
-nodes.append([0, 0])
-nodes.append([120, 0])
-nodes.append([240, 0])
-nodes.append([360, 0])
+    def addBar(self,Node1, Node2, CrossSectionArea):
+        self.Bars.append([Node1, Node2])
+        self.Area.append(CrossSectionArea)
 
-bars.append([0, 1])
-bars.append([1, 2])
-bars.append([2, 3])
-bars.append([4, 5])
-bars.append([5, 6])
-bars.append([6, 7])
+    def addAppliedForce(self, Node, Fx, Fy):
+        self.appliedForces.append([Node, Fx, Fy])
 
-bars.append([5, 1])
-bars.append([6, 2])
-bars.append([7, 3])
+    def addSupport(self, Node, Fx, Fy):
+        self.supports.append([Node, Fx, Fy])
 
-bars.append([0, 5])
-bars.append([4, 1])
-bars.append([1, 6])
-bars.append([5, 2])
-bars.append([2, 7])
-bars.append([7, 3])
+    def solve(self):
+        nodes = np.array(self.Nodes).astype(float)
+        bars = np.array(self.Bars)
+        A = np.array(self.Area)
 
-nodes = np.array(nodes).astype(float)
-bars = np.array(bars)
+        # Applied forces
+        P = np.zeros_like(nodes)
+        for i in range(len(self.appliedForces)):
+            P[self.appliedForces[i][0], 0] = self.appliedForces[i][1]
+            P[self.appliedForces[i][0], 1] = self.appliedForces[i][2]
 
-# Applied forces
-P = np.zeros_like(nodes)
-P[7, 1] = -10
+        # Supports
+        DOFCON = np.ones_like(nodes).astype(int)
+        Ur = []
+        for i in range(len(self.supports)):
+            if self.supports[i][1] == 0:
+                DOFCON[self.supports[i][0], 0] = 0
+                Ur.append(0)
+            if self.supports[i][2] == 0:
+                DOFCON[self.supports[i][0], 1] = 0
+                Ur.append(0)
 
-# Support Displacement
-Ur = [0, 0, 0, 0]
+        NN = len(nodes)
+        NE = len(bars)
+        DOF = 2
+        NDOF = DOF * NN
 
-# Condition of DOF (1 = free, 0 = fixed)
-DOFCON = np.ones_like(nodes).astype(int)
-DOFCON[0, :] = 0
-DOFCON[4, :] = 0
+        # structural analysis
+        d = nodes[bars[:, 1], :] - nodes[bars[:, 0], :]
+        L = np.sqrt((d ** 2).sum(axis=1))
+        angle = d.T / L
+        a = np.concatenate((-angle.T, angle.T), axis=1)
+        K = np.zeros([NDOF, NDOF])
+        for k in range(NE):
+            aux = 2 * bars[k, :]
+            index = np.r_[aux[0]:aux[0] + 2, aux[1]:aux[1] + 2]
 
+            ES = np.dot(a[k][np.newaxis].T * self.E * A[k], a[k][np.newaxis]) / L[k]
+            K[np.ix_(index, index)] = K[np.ix_(index, index)] + ES
 
-# %% Truss structural analysis
-def TrussAnalysis():
-    NN = len(nodes)
-    NE = len(bars)
-    DOF = 2
-    NDOF = DOF * NN
+        freeDOF = DOFCON.flatten().nonzero()[0]
+        supportDOF = (DOFCON.flatten() == 0).nonzero()[0]
+        Kff = K[np.ix_(freeDOF, freeDOF)]
+        Kfr = K[np.ix_(freeDOF, supportDOF)]
+        Krf = Kfr.T
+        Krr = K[np.ix_(supportDOF, supportDOF)]
+        Pf = P.flatten()[freeDOF]
+        Uf = np.linalg.solve(Kff, Pf)
+        U = DOFCON.astype(float).flatten()
+        U[freeDOF] = Uf
+        U[supportDOF] = Ur
+        U = U.reshape(NN, DOF)
+        u = np.concatenate((U[bars[:, 0]], U[bars[:, 1]]), axis=1)
+        N = self.E * A[:] / L[:] * (a[:] * u[:]).sum(axis=1)
+        R = (Krf[:] * Uf).sum(axis=1) + (Krr[:] * Ur).sum(axis=1)
+        self.Forces = np.array(N)[np.newaxis].T
+        self.Reactions = np.array(R)
+        self.Displacements = U
+        return self.Forces, self.Reactions, self.Displacements
 
-    # structural analysis
-    d = nodes[bars[:, 1], :] - nodes[bars[:, 0], :]
-    L = np.sqrt((d ** 2).sum(axis=1))
-    angle = d.T / L
-    a = np.concatenate((-angle.T, angle.T), axis=1)
-    K = np.zeros([NDOF, NDOF])
-    for k in range(NE):
-        aux = 2 * bars[k, :]
-        index = np.r_[aux[0]:aux[0] + 2, aux[1]:aux[1] + 2]
+    def Plot(self, nodes, bars, c, lt, lw, lg):
+        for i in range(len(bars)):
+            xi, xf, = nodes[bars[i, 0], 0], nodes[bars[i, 1], 0]
+            yi, yf, = nodes[bars[i, 0], 1], nodes[bars[i, 1], 1]
+            line, = plt.plot([xi, xf], [yi, yf], color=c, linestyle=lt, linewidth=lw)
+        line.set_label(lg)
+        plt.legend()
 
-        ES = np.dot(a[k][np.newaxis].T * E * A, a[k][np.newaxis]) / L[k]
-        K[np.ix_(index, index)] = K[np.ix_(index, index)] + ES
+    def print(self, scale):
+        print('Axial Forces (positive = tension, negative = compression)')
+        print(self.Forces)
+        print('Reaction Forces (positive = upward, negative = downword)')
+        print(self.Reactions)
+        print('Deformation at nodes')
+        print(self.Displacements)
 
-    freeDOF = DOFCON.flatten().nonzero()[0]
-    supportDOF = (DOFCON.flatten() == 0).nonzero()[0]
-    Kff = K[np.ix_(freeDOF, freeDOF)]
-    Kfr = K[np.ix_(freeDOF, supportDOF)]
-    Krf = Kfr.T
-    Krr = K[np.ix_(supportDOF, supportDOF)]
-    Pf = P.flatten()[freeDOF]
-    Uf = np.linalg.solve(Kff, Pf)
-    U = DOFCON.astype(float).flatten()
-    U[freeDOF] = Uf
-    U[supportDOF] = Ur
-    U = U.reshape(NN, DOF)
-    u = np.concatenate((U[bars[:, 0]], U[bars[:, 1]]), axis = 1)
-    N = E * A / L[:] * (a[:] * u[:]).sum(axis=1)
-    R = (Krf[:] * Uf).sum(axis=1) + (Krr[:] * Ur).sum(axis=1)
-    R = R.reshape(2, DOF)
-    return np.array(N), np.array(R), U
+        nodes = np.array(self.Nodes).astype(float)
+        bars = np.array(self.Bars)
+        self.Plot(nodes, bars, 'gray', '--', 1, 'Undeformed')
+        Dnodes = self.Displacements * scale + nodes
+        self.Plot(Dnodes, bars, 'red', '-', 2, 'deformed')
+        plt.show()
 
+truss = Truss()
+truss.addNode(0, 120)
+truss.addNode(120, 120)
+truss.addNode(240, 120)
+truss.addNode(360, 120)
+truss.addNode(0, 0)
+truss.addNode(120, 0)
+truss.addNode(240, 0)
+truss.addNode(360, 0)
 
-def Plot(nodes, c, lt, lw, lg):
-    for i in range(len(bars)):
-        xi, xf, = nodes[bars[i, 0], 0], nodes[bars[i, 1], 0]
-        yi, yf, = nodes[bars[i, 0], 1], nodes[bars[i, 1], 1]
-        line, = plt.plot([xi, xf], [yi, yf], color=c, linestyle=lt, linewidth=lw)
-    line.set_label(lg)
-    plt.legend()
+truss.addBar(0,1,0.111)
+truss.addBar(1,2,0.111)
+truss.addBar(2,3,0.111)
+truss.addBar(4,5,0.111)
+truss.addBar(5,6,0.111)
+truss.addBar(6,7,0.111)
 
-# %% Result
-N, R, U = TrussAnalysis()
-print('Axial Forces (positive = tension, negative = compression)')
-print(N[np.newaxis].T)
-print('Reaction Forces (positive = upward, negative = downword)')
-print(R)
-print('Deformation at nodes')
-print(U)
-Plot(nodes, 'gray', '--', 1, 'Undeformed')
-scale = 1
-Dnodes = U * scale + nodes
-Plot(Dnodes, 'red', '-', 2, 'deformed')
-plt.show()
+truss.addBar(5,1,0.111)
+truss.addBar(6,2,0.111)
+truss.addBar(7,3,0.111)
+
+truss.addBar(0,5,0.111)
+truss.addBar(4,1,0.111)
+truss.addBar(1,6,0.111)
+truss.addBar(5,2,0.111)
+truss.addBar(2,7,0.111)
+truss.addBar(7,3,0.111)
+
+truss.addAppliedForce(7,0,-10)
+
+truss.addSupport(0,0,0)
+truss.addSupport(4,0,0)
+
+truss.solve()
+
+truss.print(1)
